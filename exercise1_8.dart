4af65c177558bb1703609fdac79c22fc1c2c2302
Sarah Méthot <methot.sarah@gmail.com>
2013-03-13 18:31:52 -0300

    Create exercise1_8.dart

---
+++ exercise1_8.dart
@@ -0,0 +1,17 @@
+part of exercise1;
+
+num x = 77;
+
+ex1_8(){
+  if (x >= 90){
+    print('$x % = A');
+  } else if (x >= 80){
+    print('$x % = B');
+  } else if (x >= 70){
+    print('$x % = C');
+  } else if (x >= 60){
+    print('$x % = D');
+  } else {
+    print ('$x % = E');
+  }
+}
