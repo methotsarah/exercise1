79df9e425d331860528ae6e6fbcf92a8bc7d1ddb
Sarah Méthot <methot.sarah@gmail.com>
2013-03-13 18:32:15 -0300

    Create exercise1_9.dart

---
+++ exercise1_9.dart
@@ -0,0 +1,8 @@
+part of exercise1;
+
+List listA = ['Jean-Michel', 'Marc', 'Vanessa', 'Anne', 'Maximilien', 'Alexandre-Benoît', 'Louise'];
+ex1_9(){
+  listA.forEach((i) {
+    print('$i : ${i.length} caractères');
+  });
+}
