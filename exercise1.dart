e2efebf1a2c57ca3e0a729948f9068045b598c14
Sarah Méthot <methot.sarah@gmail.com>
2013-03-13 18:33:05 -0300

    Create exercise1.dart

---
+++ exercise1.dart
@@ -0,0 +1,48 @@
+library exercise1;
+
+import 'dart:math';
+
+part 'exercise1_1.dart';
+part 'exercise1_2.dart';
+part 'exercise1_3.dart';
+part 'exercise1_4.dart';
+part 'exercise1_5.dart';
+part 'exercise1_6.dart';
+part 'exercise1_7.dart';
+part 'exercise1_8.dart';
+part 'exercise1_9.dart';
+part 'exercise1_10.dart';
+
+void main(){
+  print('Exercise 1 par Sarah Méthot - 111 007 952');
+  print('');
+  print('exercise 1.1:');
+  ex1_1();
+  print('');
+  print('exercise 1.2:');
+  ex1_2();
+  print('');
+  print('exercise 1.3:');
+  ex1_3();
+  print('');
+  print('exercise 1.4:');
+  ex1_4();
+  print('');
+  print('exercise 1.5:');
+  ex1_5();
+  print('');
+  print('exercise 1.6:');
+  ex1_6();
+  print('');
+  print('exercise 1.7:');
+  ex1_7();
+  print('');
+  print('exercise 1.8:');
+  ex1_8();
+  print('');
+  print('exercise 1.9:');
+  ex1_9();
+  print('');
+  print('exercise 1.10:');
+  ex1_10();
+}
