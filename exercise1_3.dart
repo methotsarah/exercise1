e18abadd37cc9f48e8c069cd4c46310754aa6b29
Sarah Méthot <methot.sarah@gmail.com>
2013-03-13 18:29:36 -0300

    Create exercise1_3.dart

---
+++ exercise1_3.dart
@@ -0,0 +1,15 @@
+part of exercise1;
+
+String s1 = '    *';
+String s2 = '   ***';
+String s3 = '  *****';
+String s4 = ' *******';
+String s5 = '*********';
+
+ex1_3() {
+  print(s1);
+  print(s2);
+  print(s3);
+  print(s4);
+  print(s5);
+}
